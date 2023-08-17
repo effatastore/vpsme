d49a57618b327ae195ad5f59417be9138cb5c74b
PETRUS CHRIST RYAN <135100701+effatastore@users.noreply.github.com>
2023-08-17 15:09:31 +0700

    Add files via upload

---
+++ more-option/antitorrent.sh
@@ -0,0 +1,58 @@
+#wget https://github.com/${GitUser}/
+GitUser="effatastore"
+#IZIN SCRIPT
+MYIP=$(curl -sS ipv4.icanhazip.com)
+echo -e "\e[32mloading...\e[0m"
+clear
+# Valid Script
+VALIDITY () {
+    today=`date -d "0 days" +"%Y-%m-%d"`
+    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
+    if [[ $today < $Exp1 ]]; then
+    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
+    else
+    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
+    echo -e "\e[31mPlease renew your ipvps first\e[0m"
+    exit 0
+fi
+}
+IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+VALIDITY
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+echo -e "\e[31mPlease buy script first\e[0m"
+exit 0
+fi
+echo -e "\e[32mloading...\e[0m"
+clear
+
+if [[ $USER != 'root' ]]; then
+	echo "Oops! root privileges needed"
+	exit
+fi
+while :
+do
+	clear
+	echo " "
+	echo " "
+	echo "-----------------------------------------"
+	echo "            Install Anti Torrent         "
+	echo "-----------------------------------------"
+	echo "Anti-torrent was already installed in your server"
+	echo "You don't need to touch anything here"
+	echo "Just sit back and relax"
+	echo " "
+	echo " "
+	echo "     [ MENU ]"
+	echo "1. Go Back"
+	read -p "Type 1 to go back : " option2
+	case $option2 in
+		1)
+		clear
+		exit
+		;;
+	esac
+done
+cd
