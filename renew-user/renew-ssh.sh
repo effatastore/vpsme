5794ca8fd32ddde8e214bb6f6df25ee21e6ebac9
PETRUS CHRIST RYAN <135100701+effatastore@users.noreply.github.com>
2023-08-17 15:10:35 +0700

    Add files via upload

---
+++ renew-user/renew-ssh.sh
@@ -0,0 +1,60 @@
+#wget https://github.com/${GitUser}/
+GitUser="effatastore"
+#IZIN SCRIPT
+MYIP=$(curl -sS ipv4.icanhazip.com)
+echo -e "\e[32mloading...\e[0m"
+clear
+# Valid Script
+VALIDITY () {
+    today=`date -d "0 days" +"%Y-%m-%d"`
+    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
+    if [[ $today < $Exp1 ]]; then
+    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
+    else
+    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
+    echo -e "\e[31mPlease renew your ipvps first\e[0m"
+    exit 0
+fi
+}
+IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+VALIDITY
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+echo -e "\e[31mPlease buy script first\e[0m"
+exit 0
+fi
+echo -e "\e[32mloading...\e[0m"
+clear
+read -p " Username :  " User
+egrep "^$User" /etc/passwd >/dev/null
+if [ $? -eq 0 ]; then
+read -p "         Day Extend     :  " Days
+Today=`date +%s`
+Days_Detailed=$(( $Days * 86400 ))
+Expire_On=$(($Today + $Days_Detailed))
+Expiration=$(date -u --date="1970-01-01 $Expire_On sec GMT" +%Y/%m/%d)
+Expiration_Display=$(date -u --date="1970-01-01 $Expire_On sec GMT" '+%d %b %Y')
+passwd -u $User
+usermod -e  $Expiration $User
+egrep "^$User" /etc/passwd >/dev/null
+echo -e "$Pass\n$Pass\n"|passwd $User &> /dev/null
+clear
+echo -e ""
+echo -e "========================================"
+echo -e ""
+echo -e "    Username        :  $User"
+echo -e "    Days Added      :  $Days Days"
+echo -e "    Expires on      :  $Expiration_Display"
+echo -e ""
+echo -e "========================================"
+else
+clear
+echo -e ""
+echo -e "======================================"
+echo -e ""
+echo -e "        Username Doesnt Exist         "
+echo -e ""
+echo -e "======================================"
+fi
