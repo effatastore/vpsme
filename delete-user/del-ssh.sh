9ab9e274b4421ac122fcd3a74c75a50e4d631ef4
PETRUS CHRIST RYAN <135100701+effatastore@users.noreply.github.com>
2023-08-17 15:06:01 +0700

    Add files via upload

---
+++ delete-user/del-ssh.sh
@@ -0,0 +1,37 @@
+#wget https://github.com/${GitUser}/
+GitUser="effatastore"
+#IZIN SCRIPT
+MYIP=$(curl -sS ipv4.icanhazip.com)
+echo -e "\e[32mloading...\e[0m"
+clear
+# Valid Script
+VALIDITY () {
+    today=`date -d "0 days" +"%Y-%m-%d"`
+    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
+    if [[ $today < $Exp1 ]]; then
+    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
+    else
+    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
+    echo -e "\e[31mPlease renew your ipvps first\e[0m"
+    exit 0
+fi
+}
+IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+VALIDITY
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+echo -e "\e[31mPlease buy script first\e[0m"
+exit 0
+fi
+echo -e "\e[32mloading...\e[0m"
+clear
+read -p "Username SSH to Delete : " Pengguna
+
+if getent passwd $Pengguna > /dev/null 2>&1; then
+        userdel $Pengguna
+        echo -e "User $Pengguna was removed."
+else
+        echo -e "Failure: User $Pengguna Not Exist."
+fi
